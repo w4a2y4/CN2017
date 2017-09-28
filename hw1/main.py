5a86965846d6a1f1f9405a4ea4eca0e949c67668
w4a2y4 <b04902079@ntu.edu.tw>
2017-09-28 17:41:06 +0800

    Read from config and change some code structure

--- hw1/main.py
+++ hw1/main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import socket
 
+IRCSocket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
 HOST = 'irc.freenode.net'
 PORT = 6667
-CHANNEL = '#CN2017'
-
-IRCSocket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-IRCSocket.connect( ( HOST, PORT ) )
+# read channel from config
+with open('config') as f:
+	CHANNEL = f.read().split('CHAN=')[1][1:-1]
 
 
 def privMsg(msg):
@@ -19,9 +19,22 @@ def sendMsg(msg):
 	IRCSocket.send( bytes(msg, 'utf-8') )
 
 
-def checkIP(ip):
+def convert(num):
+	if( num.startswith('0x') ):
+		try:
+			privMsg(str(int(num, 16)))
+		except Exception:
+			privMsg('invalid input OAQ')
+	else:
+		try:
+			privMsg(hex(int(num)))
+		except Exception:
+			privMsg('invalid input OAQ')
 
-	ln = len(ip)
+
+def ip(s):
+
+	ln = len(s)
 	if( ln < 4 or ln > 12 ):
 		privMsg('0')
 		return
@@ -30,10 +43,10 @@ def checkIP(ip):
 	for i in range(1, 4):
 		for j in range(i+1, min(ln, i+4)):
 			for k in range(j+1, min(ln, j+4)):
-				a = ip[:i]
-				b = ip[i:j]
-				c = ip[j:k]
-				d = ip[k:]
+				a = s[:i]
+				b = s[i:j]
+				c = s[j:k]
+				d = s[k:]
 				addr = a + '.' + b + '.' + c + '.' + d
 				try:
 					socket.inet_aton(addr)
@@ -48,6 +61,13 @@ def checkIP(ip):
 
 def main():
 
+	# connect to IRC
+	try:
+		IRCSocket.connect(( HOST, PORT ))
+	except Exception:
+		print('Failed to connect :(')
+		return
+
 	sendMsg('USER imthebot imthebot imthebot imthebot')
 	sendMsg('NICK imthebot')
 	sendMsg('JOIN ' + CHANNEL)
@@ -55,8 +75,7 @@ def main():
 
 	while True :
 		IRCMsg = str(IRCSocket.recv( 4096 ).decode('utf-8'))
-		if( not IRCMsg.isspace() ):
-			print(IRCMsg)
+		print(IRCMsg)
 
 		# ping-pong
 		if( IRCMsg.find('PING') != -1 ):
@@ -75,17 +94,7 @@ def main():
 				privMsg(text.split('@repeat ', 1)[1])
 
 			elif( text.startswith('@convert ') ):
-				num = text.split('@convert ', 1)[1]
-				if( num.startswith('0x') ):
-					try:
-						privMsg(str(int(num, 16)))
-					except Exception:
-						privMsg('invalid input OAQ')
-				else:
-					try:
-						privMsg(hex(int(num)))
-					except Exception:
-						privMsg('invalid input OAQ')
+				concert(text.split('@convert ', 1)[1])
 
 			elif( text.startswith('@ip ') ):
 				checkIP(text.split('@ip ', 1)[1])
