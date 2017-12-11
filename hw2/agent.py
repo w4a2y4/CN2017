36dfd2d4303eab5407e0a6e97dd4c97e4db404d3
Chih-Wei Ning <b04902079@ntu.edu.tw>
2017-12-11 12:56:51 +0800

    init hw2

---
+++ hw2/agent.py
@@ -0,0 +1,20 @@
+import socket
+
+# connect with recver
+recv_addr = ('127.0.0.1', 31500) 
+recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+
+# create socket between sender & agent
+send_addr = ('127.0.0.1', 31600)  
+send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
+send_socket.bind(send_address) 
+
+def main():
+
+
+
+
+
+if __name__ == "__main__":
+
+  main()
---
+++ hw2/sender.py
@@ -0,0 +1,30 @@
+import socket
+
+# constants
+rtt = 1
+payload = 1024
+file_path = ""
+
+# connect with agent
+send_addr = ('127.0.0.1', 31600)  
+send_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def sndpkt(num):
+	data = 
+
+def timeout(num):
+
+def main():
+
+	send_base = 0
+	next_seq_num = 0
+	sndpkt(0);
+
+	while True:
+		pkt = 
+
+
+
+if __name__ == "__main__":
+
+  main()
