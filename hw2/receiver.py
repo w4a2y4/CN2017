887be4232be3c62fdefac6e4087bfef133dda928
Chih-Wei Ning <b04902079@ntu.edu.tw>
2017-12-11 16:25:40 +0800

    Revise agent & recver for test purpose

--- hw2/receiver.py
+++ hw2/receiver.py
@@ -1,14 +1,15 @@
 import socket
 
 # create socket between recver & agent
-recv_addr = ('127.0.0.1', 31500)  
-recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  
-recv_socket.bind(recv_address) 
+recv_addr = ('127.0.0.1', 31500)
+recv_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+recv_socket.bind(recv_address)
 
 def main():
 
-
+	while True:
+		pass
 
 if __name__ == "__main__":
 
-  main()
+	main()
